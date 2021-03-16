79c6b0bec31f2e659d3419c75a7127a1021d1383
Tony Tran <tuan.pfiev@gmail.com>
2021-03-16 18:42:50 +1100

    gitignore

--- balloon1.sh
+++ balloon1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -geometry 90x30+200+00 -title "IMU" -hold -e "cd ~/HAB/XSENS_Test/build; ./XSENS_Test"&
+#xterm -geometry 90x30+200+00 -title "IMU" -hold -e "cd ~/HAB/XSENS_Test/build; ./XSENS_Test"&
 sleep 0.5; xterm -geometry 90x30+800+0 -title "GPS Logger" -hold -e "cd ~/HAB/GPS-Loggers; python3 GPSLoggerUART.py" &
 sleep 1; xterm -geometry 90x30+1400+0 -title "Network" -hold -e "cd ~/HAB/Simple-RFD900-Network; python3 RadioNetworkMain.py"&
 sleep 3; xterm -geometry 90x30+800+450 -title "Localisation" -hold -e "cd ~/HAB/localisation; python3 localisation_main.py"&
