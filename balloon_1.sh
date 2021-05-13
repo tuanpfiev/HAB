f169ec3d96c249e972f79cd4f9b576171d804e4f
Tony Tran <tuan.pfiev@gmail.com>
2021-05-13 15:09:36 +1000

    working

--- balloon_1.sh
+++ balloon_1.sh
@@ -11,7 +11,7 @@ sleep 1; xterm  -title "Thermal log" -hold -e "cd ~/HAB/Thermal_Monitor; python3
 sleep 1.1; xterm -title "TemperatureSensor" -hold -e "cd ~/HAB/temperatureSensor; python3 temperatureSensor.py $1" &
 sleep 2; xterm  -title "GPS Logger" -hold -e "cd ~/HAB/GPS-Loggers; python3 GPSLoggerUART.py /dev/ttyUSB2" &
 sleep 3; xterm  -title "Network" -hold -e "cd ~/HAB/Simple-RFD900-Network; python3 RadioNetworkMain.py $1"&
-sleep 4; xterm  -title "Lora1" -hold -e "cd ~/HAB/LoRa-Radio-RSSI; python3 LoraRadio.py start /dev/ttyUSB0 ${neighborBalloon[0]} $1" &
+sleep 4; xterm  -title "Lora1" -hold -e "cd ~/HAB/LoRa-Radio-RSSI-Thread; python3 LoraRadio.py start /dev/ttyUSB0 ${neighborBalloon[0]} $1" &
 sleep 4.1; xterm  -title "Lora2" -hold -e "cd ~/HAB/LoRa-Radio-RSSI; python3 LoraRadio.py wait /dev/ttyUSB1 ${neighborBalloon[1]} $1" &
 sleep 6; xterm -title "EKF" -hold -e "cd ~/HAB/EKF-3Balloons; python3 main_EKF.py $1"&
 sleep 7; xterm -title "Localisation" -hold -e "cd ~/HAB/localisation-RSSI; python3 localisation_main.py $1"
