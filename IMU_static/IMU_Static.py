bba20f6aae4559c0c91deb5c6f8e1ea1d4f34236
tuanpfiev <tuan.pfiev@gmail.com>
2021-05-20 20:29:39 +1000

    working

--- IMU_static/IMU_Static.py
+++ IMU_static/IMU_Static.py
@@ -4,7 +4,7 @@ s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 while True:
     epoch = time.time()
-    send_data = "{|SYSTEM_ID: 1; EPOCH: " + str(epoch) + "; ACCELERATION: 0,0,9.81; GYRO: 0,0,0; MAGNETIC_VECTOR: 0,0,0; RAW_QT: 0,0,0,0; EULER_321: 0,0,0;}"
+    send_data = "{|SYSTEM_ID: 1; EPOCH: " + str(epoch) + "; ACCELERATION: 0,0,9.81; GYRO: 0,0,0; MAGNETIC_VECTOR: 0,0,0; RAW_QT: 0,0,0,1; EULER_321: 0,0,0;}"
     print(send_data)
     data_encoded = send_data.encode('utf-8')
 
